be1d1174a84eb6dccc1f2d0f7bf401e289fef2f4
Craig Condit <ccondit@randomcoder.org>
2006-07-07 17:32:38 +0000

    Initial import
    git-svn-id: https://randomcoder.org/repos/public/taglibs/trunk@5 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ src/com/randomcoder/taglibs/security/NotInRoleTag.java
@@ -0,0 +1,61 @@
+/*
+ * $Id: NotInRoleTag.java 6 2005-10-15 18:08:53Z ccondit $
+ */
+package com.randomcoder.taglibs.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+/**
+ * Tag class which implements (not) isUserInRole for JSP files.
+ * 
+ * <pre>
+ * Copyright (c) 2006, Craig Condit. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *     
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * </pre> 
+ */
+public class NotInRoleTag extends BodyTagSupport {
+  private static final long serialVersionUID = 1471749627660813962L;
+
+  private String role;
+
+  /**
+   * Sets the role to test
+   * @param role role name
+   */
+  public void setRole(String role) { this.role = role; }
+
+  @Override
+  public int doStartTag() throws JspException {
+    HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+
+    String[] roles = role.split(",");
+    for (int i = 0; i < roles.length; i++) {
+      if (request.isUserInRole(roles[i]))
+        return SKIP_BODY;
+    }
+
+    return EVAL_BODY_INCLUDE;
+  }
+}
