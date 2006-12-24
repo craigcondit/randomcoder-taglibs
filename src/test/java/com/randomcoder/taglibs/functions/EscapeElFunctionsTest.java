7f28f2d001e1fd215dff57ccd6c973752e6f1a9a
Craig Condit <ccondit@randomcoder.org>
2006-12-24 00:58:04 +0000

    Added test cases for EscapeElFunctions
    git-svn-id: https://randomcoder.org/repos/public/taglibs/trunk@483 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ src/test/java/com/randomcoder/taglibs/functions/EscapeElFunctionsTest.java
@@ -0,0 +1,32 @@
+package com.randomcoder.taglibs.functions;
+
+import junit.framework.TestCase;
+
+public class EscapeElFunctionsTest extends TestCase
+{
+	public void testUrlencode()
+	{
+		assertNull(EscapeElFunctions.urlencode(null));
+	}
+	
+	public void testUrlencodeNull()
+	{
+		assertEquals("Testing+%26+%3C+%3E", EscapeElFunctions.urlencode("Testing & < >"));
+	}
+	
+	public void testAttribute()
+	{
+		assertEquals("Testing &amp; &lt; &gt; &#34; &#39;", EscapeElFunctions.attribute("Testing & < > \" '"));
+	}
+
+	public void testJsDouble()
+	{
+		assertEquals("Testing \\\\ \\\"", EscapeElFunctions.jsDouble("Testing \\ \""));
+	}
+
+	public void testJsSingle()
+	{
+		assertEquals("Testing \\\\ \\'", EscapeElFunctions.jsSingle("Testing \\ '"));
+	}
+
+}
