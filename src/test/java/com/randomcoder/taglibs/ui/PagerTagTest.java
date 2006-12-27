ad51b09557c48aebb9a650d6e01a4a85b2df40d2
Craig Condit <ccondit@randomcoder.org>
2006-12-27 04:52:39 +0000

    Added test cases for PagerTag
    git-svn-id: https://randomcoder.org/repos/public/taglibs/trunk@500 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ src/test/java/com/randomcoder/taglibs/ui/PagerTagTest.java
@@ -0,0 +1,73 @@
+package com.randomcoder.taglibs.ui;
+
+import com.randomcoder.taglibs.test.base.AbstractTagTestCase;
+
+public class PagerTagTest extends AbstractTagTestCase
+{
+	private PagerTag tag;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		tag = new PagerTag();
+		super.setUp(tag);
+		tag.setCount(50);
+		tag.setLimit(10);
+		tag.setLimitParam("limit");
+		tag.setLink("http://www.example.com/");
+		tag.setMaxLinks(3);
+		tag.setNextContent("&gt;");
+		tag.setPrevContent("&lt;");
+		tag.setStart(0);
+		tag.setStartParam("start");
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		tag.release();
+		tag = null;
+		super.tearDown();
+	}
+
+	public void testTagCurrentUrl() throws Exception
+	{
+		request.setQueryString(null);
+		request.setServerName("www.example.com");
+		request.setContextPath("");
+		request.setServletPath("/page");
+		request.setPathInfo(null);
+		tag.setLink(null);
+		tag.doStartTag();
+		tag.doEndTag();
+		String result = writer.toString();
+		assertEquals("1 <a href=\"/page?limit=10&amp;start=10\">2</a> <a href=\"/page?limit=10&amp;start=20\">3</a> <a href=\"/page?limit=10&amp;start=10\">&gt;</a>", result);
+	}
+	
+	public void testTagFirstPage() throws Exception
+	{
+		tag.doStartTag();
+		tag.doEndTag();
+		String result = writer.toString();
+		assertEquals("1 <a href=\"http://www.example.com/?limit=10&amp;start=10\">2</a> <a href=\"http://www.example.com/?limit=10&amp;start=20\">3</a> <a href=\"http://www.example.com/?limit=10&amp;start=10\">&gt;</a>", result);
+	}
+
+	public void testTagNegativeStart() throws Exception
+	{
+		tag.setStart(-1);
+		tag.doStartTag();
+		tag.doEndTag();
+		String result = writer.toString();
+		assertEquals("1 <a href=\"http://www.example.com/?limit=10&amp;start=10\">2</a> <a href=\"http://www.example.com/?limit=10&amp;start=20\">3</a> <a href=\"http://www.example.com/?limit=10&amp;start=10\">&gt;</a>", result);
+	}
+
+	public void testTagSecondPage() throws Exception
+	{		
+		tag.setStart(10);
+		tag.doStartTag();
+		tag.doEndTag();
+		String result = writer.toString();
+		assertEquals("<a href=\"http://www.example.com/?limit=10&amp;start=0\">&lt;</a> <a href=\"http://www.example.com/?limit=10&amp;start=0\">1</a> 2 <a href=\"http://www.example.com/?limit=10&amp;start=20\">3</a> <a href=\"http://www.example.com/?limit=10&amp;start=30\">4</a> <a href=\"http://www.example.com/?limit=10&amp;start=20\">&gt;</a>", result);
+	}
+	
+}
